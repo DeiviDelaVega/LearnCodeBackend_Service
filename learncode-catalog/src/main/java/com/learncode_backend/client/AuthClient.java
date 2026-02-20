1c13e8cac6dc8d10695201d1237e2c9643d109db
GiacomoDel <giacomo.delgado@acchiwan.pe>
2026-02-20 01:02:34 -0500

    ContentClient_Fix

--- learncode-catalog/src/main/java/com/learncode_backend/client/AuthClient.java
+++ learncode-catalog/src/main/java/com/learncode_backend/client/AuthClient.java
@@ -3,12 +3,12 @@ package com.learncode_backend.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.learncode_backend.config.FeignConfig;
 import java.util.UUID;
 
-
-@FeignClient(name = "AUTH-SERVICE") 
+@FeignClient(name = "AUTH-SERVICE", configuration = FeignConfig.class) 
 public interface AuthClient {
 
-    @GetMapping("/api/admin/gestionCliente/id/{email}") 
+    @GetMapping("/api/auth/users/id/{email}") 
     UUID getUserIdByEmail(@PathVariable("email") String email);
 }
